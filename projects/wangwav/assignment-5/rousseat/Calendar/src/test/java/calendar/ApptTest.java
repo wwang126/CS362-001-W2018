4f7e36d05dbcf181dae1e070670253cb4070dd3b
wwang126 <wavelet.wang@gmail.com>
2018-03-12 19:01:34 -0700

    added tests from assignment 3

--- projects/wangwav/assignment-5/rousseat/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/wangwav/assignment-5/rousseat/Calendar/src/test/java/calendar/ApptTest.java
@@ -12,10 +12,36 @@ public class ApptTest {
      */
 	 @Test
 	  public void test01()  throws Throwable  {
+		 int startHour=21;
+		 int startMinute=30;
+		 int startDay=15;
+		 int startMonth=01;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+	// assertions
+		 assertTrue(appt.getValid());
+		 assertEquals(21, appt.getStartHour());
+		 assertEquals(30, appt.getStartMinute());
+		 assertEquals(15, appt.getStartDay());
+		 assertEquals(01, appt.getStartMonth());
+		 assertEquals(2018, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());         		
 	 }
 
 	 @Test
 	  public void test02()  throws Throwable  {
 		 
-	 }	
+	 }
+//add more unit tests as you needed
+	
 }
