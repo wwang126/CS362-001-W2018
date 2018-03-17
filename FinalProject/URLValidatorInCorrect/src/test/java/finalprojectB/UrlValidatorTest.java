45b973cf173de3cc50df7d73768b870d94d7983d
wwang126 <wavelet.wang@gmail.com>
2018-03-16 18:20:46 -0700

    added some more tests

--- FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -25,7 +25,65 @@ public class UrlValidatorTest extends TestCase {
        String testString = "http://www.google.com";
        boolean result = urlTest.isValid(testString);
        assertEquals(true,result);
-       System.out.println("Passed " + testString);
+       System.out.println("Passed " + testString + " as " + result);
+       //True Tests
+       testString = "http://www.google.com:80/test1?action=view";
+       result = urlTest.isValid(testString);
+       assertEquals(true,result);
+       System.out.println("Passed " + testString + " as " + result);
+
+       testString = "ftp://0.0.0.0:0?action=edit&mode=up";
+       result = urlTest.isValid(testString);
+       assertEquals(true,result);
+       System.out.println("Passed " + testString + " as " + result);
+
+       testString = "h3t://go.au:80/test1";
+       result = urlTest.isValid(testString);
+       assertEquals(true,result);
+       System.out.println("Passed " + testString + " as " + result);
+
+       testString = "h3t://go.tv:80/test1";
+       result = urlTest.isValid(testString);
+       assertEquals(true,result);
+       System.out.println("Passed " + testString + " as " + result);
+
+       testString = "h3t://go.se:80/test1";
+       result = urlTest.isValid(testString);
+       assertEquals(true,result);
+       System.out.println("Passed " + testString + " as " + result);
+
+       testString = "ftp://google.com";
+       result = urlTest.isValid(testString);
+       assertEquals(true,result);
+       System.out.println("Passed " + testString + " as " + result);
+
+       testString = "http://ww.google.com";
+       result = urlTest.isValid(testString);
+       assertEquals(true,result);
+       System.out.println("Passed " + testString + " as " + result);
+
+       //False Tests
+       testString = "http:/www.google.com";
+       result = urlTest.isValid(testString);
+       assertEquals(false,result);
+       System.out.println("Passed " + testString + " as " + result);
+
+       testString = "ft:/www.google.com";
+       result = urlTest.isValid(testString);
+       assertEquals(false,result);
+       System.out.println("Passed " + testString + " as " + result);
+
+       testString = "www.google.com";
+       result = urlTest.isValid(testString);
+       assertEquals(false,result);
+       System.out.println("Passed " + testString + " as " + result);
+
+       testString = "www.google.com";
+       result = urlTest.isValid(testString);
+       assertEquals(false,result);
+       System.out.println("Passed " + testString + " as " + result);
+
+
 
    }
    
